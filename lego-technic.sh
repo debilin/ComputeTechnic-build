50749cdfa35325c5d159ed4705a21577990f39bc
debilin <croninlucio@gmail.com>
2024-01-15 12:35:58 -0800

    Update lego-technic.sh

--- lego-technic.sh
+++ lego-technic.sh
@@ -5,24 +5,13 @@ project=compute-technic
 containerid=$project-build-$randstr
 imageid=$project-build-$(id -u)
 
-if [ "$1" = "-path_select" ]; then
-    mkdir -p $PWD/output/vis_edges/
-    docker run \
-        --name $containerid \
-        -w /ComputeTechnic/build \
-        -v "$PWD/output:/ComputeTechnic/LEGO_Technic_data/1_debug" \
-        -v "$PWD/output/vis_edges:/ComputeTechnic/python" \
-        $imageid /ComputeTechnic/build/lego_technic_main $1 $2
-fi
-
-if [ "$1" = "-path_provide" ]; then
-    mkdir -p $PWD/vis_edges/
-    docker run \
-        --name $containerid \
-        -v "$PWD:/ComputeTechnic/LEGO_Technic_data/1_debug" \
-        -v "$PWD/vis_edges:/ComputeTechnic/python" \
-        $imageid /ComputeTechnic/build/lego_technic_main $1 $2
-fi
+mkdir -p $PWD/output/vis_edges/
+docker run \
+    --name $containerid \
+    -w /ComputeTechnic/build \
+    -v "$PWD/output:/ComputeTechnic/LEGO_Technic_data/1_debug" \
+    -v "$PWD/output/vis_edges:/ComputeTechnic/python" \
+    $imageid /ComputeTechnic/build/lego_technic_main $1 $2
 
 if [ "$1" = "-vis_edges" ]; then
     lego_vis_edges $2
