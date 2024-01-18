7f9b3cb51fe7dd988e162440346b4d0d22df667c
debilin <croninlucio@gmail.com>
2024-01-18 09:38:15 -0800

    Use docker build --target in GitHub Action.

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
-source build_deb.sh
+randstr=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+project=lego-technic
+containerid=$project-build-$randstr
+imageid=$project-build-$(id -u)
+
+(set -xe; podman build --target builder --progress=plain -t $imageid .)
+
+set -xe
+
+podman run \
+    --name $containerid \
+    $imageid
+
+current_script=$( dirname "$(readlink -f "$0")" )
+podman cp $containerid:/dist $current_script
+podman rm $containerid
 
 (set -xe; podman build -t $project .)
